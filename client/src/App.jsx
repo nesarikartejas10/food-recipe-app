528a14a1e2a2c8a3de1fe99862ee49377d7ce8be
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-16 13:26:07 +0530

    configure routing in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import RootLayout from "./loyouts/RootLayout";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
-      <div>App</div>
+      <RouterProvider router={router} />
     </>
   );
 }
