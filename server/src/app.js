5ed442c76302a7887bbecdd3077ec7579572e742
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-16 11:52:47 +0530

    configure CORS to allow requests from frontend URL

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,18 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import recipeRouter from "./routes/recipe.routes.js";
+import cors from "cors";
+import { config } from "./config/envConfig.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: config.frontendURL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/v1/recipe", recipeRouter);
