7882839751840473c9bbca8329818281df6cd68b
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-16 08:40:37 +0530

    create recipe schema model

---
+++ server/src/models/recipe.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+      minlength: [3, "Title must be at least 3 characters"],
+      maxlength: [50, "Title cannot exceed 50 characters"],
+    },
+
+    ingredients: {
+      type: [String],
+      required: [true, "Ingredients are required"],
+      validate: {
+        validator: function (arr) {
+          return arr && arr.length > 0;
+        },
+        message: "At least one ingredient is required",
+      },
+    },
+
+    instructions: {
+      type: String,
+      required: [true, "Instructions are required"],
+      trim: true,
+      minlength: [10, "Instructions must be detailed (at least 10 characters)"],
+    },
+
+    cookingTime: {
+      type: Number,
+      required: [true, "Cooking time is required"],
+      min: [1, "Cooking time must be at least 1 minute"],
+      max: [240, "Cooking time cannot exceed 4 hours"],
+    },
+
+    coverImage: {
+      type: String,
+      default: null,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Creator information is required"],
+    },
+  },
+  { timestamps: true },
+);
+
+export const Recipe = mongoose.model("Recipe", recipeSchema);
