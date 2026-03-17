c566e1680466b596cd83b3284e12b18ee47c0826
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-17 20:11:56 +0530

    add registerUser with validation, hashing, and JWT token

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,6 +1,34 @@
+import createHttpError from "http-errors";
 import asyncHandler from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { config } from "../config/envConfig.js";
 
-export const registerUser = asyncHandler(async () => {});
+export const registerUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return next(createHttpError(400, "Email already exist"));
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ email, password: hashedPassword });
+  const token = jwt.sign({ id: newUser._id, email }, config.jwtSecret, {
+    expiresIn: "1hr",
+  });
+  return res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    token,
+    user: { email: newUser.email, id: newUser._id },
+  });
+});
 
 export const loginUser = asyncHandler(async () => {});
 
